ca931ba0cbe0303fd4dd7c87658cc0fe4639f01c
massyvipin <166917148+massyvipin@users.noreply.github.com>
2025-01-28 12:26:14 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,9 +1,25 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from "react";
 
-const UseMemoComponent = ({ tasks }) => {
-  const taskCount = useMemo(() => tasks.length, [tasks]);
+const UseMemo = () => {
+  const [count, setCount] = useState(0);
 
-  return <div>Total tasks: {taskCount}</div>;
+  const result = useMemo(() => {
+    return count * 2;
+  }, [count]);
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <div>
+        <span>Count:{count}</span>
+        <button onClick={onIncrease}>+</button>
+      </div>
+      <h2>Expensive Calculation</h2>
+      {result}
+    </div>
+  );
 };
 
-export default UseMemoComponent;
+export default UseMemo;
